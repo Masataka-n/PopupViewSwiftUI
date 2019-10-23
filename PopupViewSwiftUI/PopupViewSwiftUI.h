3b651bf21ccbc48f7e8017e837e2ab0ec56b64b8
Masataka Nakagawa <n.masataka.hal.ih@gmail.com>
2019-10-23 15:29:19 +0900

    Initial Commit

---
+++ PopupViewSwiftUI/PopupViewSwiftUI.h
@@ -0,0 +1,19 @@
+//
+//  PopupViewSwiftUI.h
+//  PopupViewSwiftUI
+//
+//  Created by 中川 正貴 on 2019/10/23.
+//  Copyright © 2019 Masataka Nakagawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PopupViewSwiftUI.
+FOUNDATION_EXPORT double PopupViewSwiftUIVersionNumber;
+
+//! Project version string for PopupViewSwiftUI.
+FOUNDATION_EXPORT const unsigned char PopupViewSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupViewSwiftUI/PublicHeader.h>
+
+
